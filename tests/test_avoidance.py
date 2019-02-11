8ab4dec3cda8ab56a7eb5451a405b9d14798930f
Johan Walles <johan.walles@gmail.com>
2019-02-11 11:26:53 +0100

    Verify that tests can succeed without rerunning

--- tests/test_avoidance.py
+++ tests/test_avoidance.py
@@ -1,8 +1,9 @@
-# -*- coding: utf-8 -*-
+import os.path
+import time
 
 
 def test_reruns_test_on_test_change(testdir):
-    # create a temporary pytest test module
+    # Create a passing test
     testdir.makepyfile(test_test="""
         def test_test():
             assert True
@@ -21,12 +22,35 @@ def test_reruns_test_on_test_change(testdir):
     assert testdir.runpytest().ret != 0
 
 
-def test_runs_test_on_code_change(testdir):
-    assert False and "Not implemented"
-
-
 def test_skips_rerun(testdir):
-    assert False and "Not implemented"
+    # Create a test that stores its runtime on disk
+    timestampfile = str(testdir.tmpdir.join("timestampfile"))
+    testdir.makepyfile(test_test="""
+        def test_test():
+            with open("{}", "w") as text_file:
+                text_file.write("Johan")
+
+            assert True
+    """.format(timestampfile))
+
+    # Test should pass
+    assert testdir.runpytest().ret == 0
+
+    # Store test run timestamp
+    t0 = os.path.getmtime(timestampfile)
+
+    # Ensure next modification time is far enough away.
+    # Do we need this? What's a good value?
+    time.sleep(1.1)
+
+    # Rerunning the test should still pass, since we haven't modified anything
+    assert testdir.runpytest().ret == 0
+
+    # FIXME: Verify that pytest says one (1) test was run
+
+    # Verify that the test pass was from the cache
+    t1 = os.path.getmtime(timestampfile)
+    assert t1 == t0
 
 
 def test_bar_fixture(testdir):
